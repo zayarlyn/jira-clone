63a9231e9cd1fd860063be382705693afc19deec
Illumi lyne <zeiyarlynn@gmail.com>
2022-10-03 20:48:01 +0630

    Delete express-backend/prisma/migrations directory

--- express-backend/prisma/migrations/20220824104703_added_project_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `project` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `descr` VARCHAR(191) NULL,
-    `repo` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
